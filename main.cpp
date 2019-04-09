8b6c212359c1be13396c0e02673457505c641392
DarkInesis <49103828+DarkInesis@users.noreply.github.com>
2019-04-09 15:16:01 +0200

    Modification main
    Modification main pour fonctionner avec les nouveaux constructeurs de taches et de categorie

--- main.cpp
+++ main.cpp
@@ -4,14 +4,23 @@
 
 #include "tache.h"
 #include "categorie.h"
+#include "menu.h"
+
 
 int main()
 {
+
 	Temps t1(6, 4, 2019);
-	Categorie C1("test", t1);
+	Categorie C1;
 	Temps dureeT1(6, 0, 0);
-	Tache T1("Amine la tache", t1, dureeT1);
+	Tache T1;
+	Tache T2;
 	C1.insererTache(T1, 0);
+	C1.insererTache(T2, 1);
 	C1.afficherListeTaches();
+	cout<<"saisir la position de la tache a supprimer"<<endl;
+	int positionsupprimer = -1;
+	cin >> positionsupprimer;
+	C1.supprimerTache(positionsupprimer);
 	return 0;
 }
